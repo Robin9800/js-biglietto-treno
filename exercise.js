cc6c8c798f9b04028a2f9f0c726537c78482a7e3
Robin9800 <robin.costanzo@gmail.com>
2022-03-06 21:01:36 +0100

    Exercise completed

--- exercise.js
+++ exercise.js
@@ -1,23 +1,26 @@
 /*Inserisci i km e la tua età*/
-const kmNum = prompt ('Inserisci il numero di chilometri che si vuole percorrere');
-const ageUser = prompt ('Inserisci la tua età');
-let totale;
+const kmNum = prompt('Inserisci il numero di chilometri che si vuole percorrere');
+const ageUser = prompt('Inserisci la tua età');
+let totale = (kmNum * 0.21);
 let sconto;
 
+
+
+
 if (ageUser < 18){
-    totale = (kmNum * 0.21) + '€';
-    sconto = 'Hai diritto al 20% di sconto';
-    console.log('hai diritto al 20% di sconto, quindi pagherai ' + totale);
-} else if (ageUser > 65){
-    totale = (kmNum * 0.21) + '€';
-    sconto = 'Hai diritto al 40% di sconto';
-    console.log('hai diritto al 40% di sconto, quindi pagherai ' + totale);
+    sconto = (totale *0.2);
+
+}   else if (ageUser >= 65){
+    sconto = (totale *0.4);
 }
-else {
-    totale = (kmNum * 0.21) + '€';
-    sconto = '';
-    console.log('Questo è quanto dovrai pagare ' + totale)
+else{
+    sconto = 0
 }
+
+let finalPrice = (totale-sconto);
+console.log('Il prezzo finale è ' + finalPrice + '€');
+
+document.getElementById('total').innerHTML = 'Il prezzo finale è ' + finalPrice + '€';
 
 
      
