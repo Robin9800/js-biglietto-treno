7775f7652f3292e608e5c2880a3d7c1da8f73f7d
Robin9800 <robin.costanzo@gmail.com>
2022-03-06 18:39:32 +0100

    Add discount

--- exercise.js
+++ exercise.js
@@ -5,15 +5,20 @@ let totale;
 let sconto;
 
 if (ageUser < 18){
-    total = (kmNum * 0.21) + '€';
+    totale = (kmNum * 0.21) + '€';
     sconto = 'Hai diritto al 20% di sconto';
+    console.log('hai diritto al 20% di sconto, quindi pagherai ' + totale);
 } else if (ageUser > 65){
-    total = (kmNum * 0.21) + '€';
+    totale = (kmNum * 0.21) + '€';
     sconto = 'Hai diritto al 40% di sconto';
+    console.log('hai diritto al 40% di sconto, quindi pagherai ' + totale);
 }
 else {
     totale = (kmNum * 0.21) + '€';
     sconto = '';
+    console.log('Questo è quanto dovrai pagare ' + totale)
 }
+
+
      
 
